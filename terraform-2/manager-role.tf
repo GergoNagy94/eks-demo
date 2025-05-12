ed9b8d8ce87599a665ce52beeeb0dadc9fb84d2e
Nagy Gergő <gergosculpture@gmail.com>
2025-05-12 11:49:55 +0200

    worker node ecr policy added

--- terraform-2/manager-role.tf
+++ terraform-2/manager-role.tf
@@ -49,7 +49,7 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "eks_admin" {
-  role = aws_iam_role.eks_admin.name
+  role       = aws_iam_role.eks_admin.name
   policy_arn = aws_iam_policy.eks_admin.arn
 }
 
@@ -77,12 +77,12 @@ resource "aws_iam_policy" "eks_assume_admin" {
 }
 
 resource "aws_iam_user_policy_attachment" "manager" {
-  user = aws_iam_user.manager.name
+  user       = aws_iam_user.manager.name
   policy_arn = aws_iam_policy.eks_assume_admin.arn
 }
 
 resource "aws_eks_access_entry" "manager" {
-  cluster_name = aws_eks_cluster.eks.name
-  principal_arn = aws_iam_role.eks_admin.arn
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_role.eks_admin.arn
   kubernetes_groups = ["gergo_admin"]
 }
